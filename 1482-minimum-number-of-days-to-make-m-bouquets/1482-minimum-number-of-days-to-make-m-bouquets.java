13249a021d6cc0122d4688afaf8c7a3facb71b98
Himanshu Singh Jeena <114165565+Himanshusinghjeena@users.noreply.github.com>
2026-08-02 17:26:41 +0530

    Time: 15 ms (97.61%), Space: 56.6 MB (99.99%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if (m <= 0 || k <= 0 || m > Integer.MAX_VALUE / k || m * k > bloomDay.length) {
+            // Ensures even with potential integer overflow, the check holds
+            return -1;
+        }
+        int low = small(bloomDay);
+        int high = max(bloomDay);
+        int ans = high;
+        
+        while(low<= high){  
+        int mid = low +(high -low)/2;
+        if(possible(bloomDay,mid,m,k)) {
+            ans=mid; 
+            high = mid-1;
+        }else{
+            low=mid+1;
+        } 
+    }
+        return ans;
+    }
+    public boolean possible(int[] arr,int day, int m ,int k){
+        int count =0; int bouque =0;
+        if(m*k > arr.length) return false;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] <= day){
+                count++;
+            }
+            else {
+                bouque += count/k;
+                count=0;
+            }
+        }
+         
+        bouque += count/k;
+        if(bouque >=m) return true;
+        return false;
+    }
+
+
+    public int small(int[] arr){
+        int min = Integer.MAX_VALUE;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]<min){
+                min= arr[i];
+            }
+        }
+        return min;
+    }
+
+    public int max(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+} 
